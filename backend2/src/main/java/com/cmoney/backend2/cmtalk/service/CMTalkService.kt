e473bd8660e08babeb7014c6a98458a13742b832
willie_chang <willie_chang@cmoney.com.tw>
2021-05-20 17:36:38 +0800

    build: 移動Chat module

--- backend2/src/main/java/com/cmoney/backend2/cmtalk/service/CMTalkService.kt
+++ backend2/src/main/java/com/cmoney/backend2/cmtalk/service/CMTalkService.kt
@@ -2,7 +2,8 @@ package com.cmoney.backend2.cmtalk.service
 
 import com.cmoney.backend2.cmtalk.service.api.TargetMediaListInfo
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface CMTalkService {
     /**
