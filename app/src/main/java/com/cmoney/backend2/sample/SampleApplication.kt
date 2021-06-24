e3b37c86eaa53ab74fa4133e8df935213120f99b
willie_chang <willie_chang@cmoney.com.tw>
2021-06-24 14:03:00 +0800

    test: 修正範例App koin定義注入

--- app/src/main/java/com/cmoney/backend2/sample/SampleApplication.kt
+++ app/src/main/java/com/cmoney/backend2/sample/SampleApplication.kt
@@ -3,6 +3,7 @@ package com.cmoney.backend2.sample
 import android.app.Application
 import com.cmoney.backend2.activity.di.activityServiceModule
 import com.cmoney.backend2.additioninformationrevisit.di.additionalInformationRevisitServiceModule
+import com.cmoney.backend2.authorization.di.authorizationServiceModule
 import com.cmoney.backend2.base.di.BACKEND2_SETTING
 import com.cmoney.backend2.base.di.backendBaseModule
 import com.cmoney.backend2.base.model.setting.Platform
@@ -12,16 +13,22 @@ import com.cmoney.backend2.cellphone.di.cellphoneServiceModule
 import com.cmoney.backend2.chat.di.chatServiceModule
 import com.cmoney.backend2.chipk.di.chipkServiceModule
 import com.cmoney.backend2.cmtalk.di.cmtalkServiceModule
+import com.cmoney.backend2.common.di.commonServiceModule
 import com.cmoney.backend2.customgroup.di.customGroupServiceModule
 import com.cmoney.backend2.dtno.di.dtnoServiceModule
 import com.cmoney.backend2.emilystock.di.emilyServiceModule
+import com.cmoney.backend2.forumocean.di.forumOceanServiceModule
 import com.cmoney.backend2.identityprovider.di.identityProviderServiceModule
 import com.cmoney.backend2.image.di.imageServiceModule
 import com.cmoney.backend2.media.di.mediaServiceModule
+import com.cmoney.backend2.mobileocean.di.mobileOceanServiceModule
+import com.cmoney.backend2.note_extension.di.noteExtensionServiceModule
 import com.cmoney.backend2.notes.di.notesServiceModule
 import com.cmoney.backend2.notification.di.notificationServiceModule
 import com.cmoney.backend2.notification2.di.notification2ServiceModule
+import com.cmoney.backend2.ocean.di.oceanServiceModule
 import com.cmoney.backend2.portal.di.portalServiceModule
+import com.cmoney.backend2.profile.di.profileServiceModule
 import com.cmoney.backend2.realtimeaftermarket.di.realtimeAfterMarketServiceModule
 import com.cmoney.backend2.sample.di.viewModule
 import com.cmoney.backend2.tickdata.di.tickDataServiceModule
@@ -46,24 +53,31 @@ class SampleApplication : Application() {
                 listOf(
                     viewModule,
                     //backend module
-                    backendBaseModule,
                     activityServiceModule,
                     additionalInformationRevisitServiceModule,
+                    authorizationServiceModule,
+                    backendBaseModule,
                     billingServiceModule,
                     cellphoneServiceModule,
                     chatServiceModule,
                     chipkServiceModule,
                     cmtalkServiceModule,
+                    commonServiceModule,
                     customGroupServiceModule,
                     dtnoServiceModule,
                     emilyServiceModule,
+                    forumOceanServiceModule,
                     identityProviderServiceModule,
                     imageServiceModule,
                     mediaServiceModule,
+                    mobileOceanServiceModule,
+                    noteExtensionServiceModule,
                     notesServiceModule,
                     notificationServiceModule,
                     notification2ServiceModule,
+                    oceanServiceModule,
                     portalServiceModule,
+                    profileServiceModule,
                     realtimeAfterMarketServiceModule,
                     tickDataServiceModule,
                     trialServiceModule,
