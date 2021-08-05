ef58658d6334e65b6a441e0b7a6985319f5c2c09
fu_chen <fu_chen@cmoney.com.tw>
2021-08-05 15:06:37 +0800

    chore: version -> 4.3.0-alpha04

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,6 +1,6 @@
 object ModuleConfig {
     // project
-    const val PROJECT_VERSION = "4.3.0-alpha03"
+    const val PROJECT_VERSION = "4.3.0-alpha04"
     // project path
     const val PROJECT_PATH = ":backend2"
     // project SDK
