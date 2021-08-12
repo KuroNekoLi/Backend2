135c6c9e1b9c22505cbae940d38d8bc994b28f69
willie_chang <willie_chang@cmoney.com.tw>
2021-08-12 09:21:25 +0800

    build: 4.3.0-alpha07 -> release

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,6 +1,6 @@
 object ModuleConfig {
     // project
-    const val PROJECT_VERSION = "4.3.0-alpha07"
+    const val PROJECT_VERSION = "4.3.0"
     // project path
     const val PROJECT_PATH = ":backend2"
     // project SDK
