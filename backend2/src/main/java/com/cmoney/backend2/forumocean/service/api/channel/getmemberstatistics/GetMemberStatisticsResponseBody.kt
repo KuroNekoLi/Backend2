9b9004b2fbeffae5d82147712fe0d9a7d3160406
jason_wang <jason_wang@cmoney.net.tw>
2021-08-25 10:54:58 +0800

    feat: GetMemberStatistics 回傳物件新增資料欄位

--- backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/channel/getmemberstatistics/GetMemberStatisticsResponseBody.kt
+++ backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/channel/getmemberstatistics/GetMemberStatisticsResponseBody.kt
@@ -5,12 +5,21 @@ import com.google.gson.annotations.SerializedName
 /**
  * 指定會員的統計資訊
  *
+ * @property memberId
  * @property totalCountArticle 總發文數
  * @property totalCountReaction 總反應數
+ * @property totalCountFollowing 總追蹤數
+ * @property totalCountFollower 總被追蹤數
  */
 data class GetMemberStatisticsResponseBody(
+    @SerializedName("memberId")
+    val memberId : Long?,
     @SerializedName("totalCountArticle")
     val totalCountArticle : Int?,
     @SerializedName("totalCountReaction")
-    val totalCountReaction : Int?
+    val totalCountReaction : Int?,
+    @SerializedName("totalCountFollowing")
+    val totalCountFollowing : Int?,
+    @SerializedName("totalCountFollower")
+    val totalCountFollower : Int?
 )
