324e7d699cb65345325b74e9e1479920a4b364b4
jason_wang <jason_wang@cmoney.net.tw>
2021-08-25 10:44:37 +0800

    feat: 調整成新版文章分類

--- backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/channel/channelname/definechannelnamebuilder/CommodityType.kt
+++ backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/channel/channelname/definechannelnamebuilder/CommodityType.kt
@@ -1,7 +1,21 @@
 package com.cmoney.backend2.forumocean.service.api.channel.channelname.definechannelnamebuilder
 
 enum class CommodityType(val text : String) {
-    Stock("台股"),
-    USStock("美股"),
-    Emerging("興櫃")
+    /**
+     * 台股
+     *
+     */
+    Stock("Stock"),
+
+    /**
+     * 美股
+     *
+     */
+    USStock("USStock"),
+
+    /**
+     * 興櫃
+     *
+     */
+    Emerging("Emerging")
 }
