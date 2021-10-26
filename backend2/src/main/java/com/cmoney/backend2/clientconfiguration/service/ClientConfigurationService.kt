bc562327e3159e2324533076f59832edd271e254
Asher <asher_wu@cmoney.com.tw>
2021-10-26 14:07:36 +0800

    feat: 添加LogDataRecorder的Annotation及測試

--- backend2/src/main/java/com/cmoney/backend2/clientconfiguration/service/ClientConfigurationService.kt
+++ backend2/src/main/java/com/cmoney/backend2/clientconfiguration/service/ClientConfigurationService.kt
@@ -1,9 +1,11 @@
 package com.cmoney.backend2.clientconfiguration.service
 
+import com.cmoney.backend2.base.model.calladapter.RecordApi
 import com.cmoney.backend2.clientconfiguration.service.api.ClientConfigResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
 interface ClientConfigurationService {
@@ -13,6 +15,7 @@ interface ClientConfigurationService {
      *
      *  @param keys Key list , ex: KOL
      */
+    @RecordApi
     @POST("ClientConfiguration/api/config/get")
     suspend fun getConfig(
         @Body keys: List<String>
