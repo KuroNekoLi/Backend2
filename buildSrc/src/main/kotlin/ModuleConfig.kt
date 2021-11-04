0214fcfdac209d76fb2f657ddc56aba3afe14290
willie_chang <willie_chang@cmoney.com.tw>
2021-11-04 16:37:37 +0800

    build: cmoney dependencies to 5.0.0, moudle version to 5.0.0

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,5 +1,5 @@
 object ModuleConfig {
-    const val PROJECT_VERSION = "5.0.0-alpha02"
+    const val PROJECT_VERSION = "5.0.0"
     //SDK
     const val COMPILE_SDK = 30
     const val BUILD_TOOLS_VERSION = "30.0.3"
