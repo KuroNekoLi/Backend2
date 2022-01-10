bf1e9d0ecf82f2dae44f56cab3af0d79e704aff8
willie_chang <willie_chang@cmoney.com.tw>
2022-01-10 08:51:22 +0800

    build: 5.10.1 -> 5.10.2

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,5 +1,5 @@
 object ModuleConfig {
-    const val PROJECT_VERSION = "5.10.1"
+    const val PROJECT_VERSION = "5.10.2"
     //SDK
     const val COMPILE_SDK = 30
     const val BUILD_TOOLS_VERSION = "30.0.3"
