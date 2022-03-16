b80a8055eacedda6999502867ed837b8e395d9ca
蔡旻芳 <fang_tsai@cmoney.com.tw>
2022-03-16 14:43:42 +0800

    fix: 修改enum comment

--- backend2/src/main/java/com/cmoney/backend2/vtwebapi/service/api/createaccount/AccountType.kt
+++ backend2/src/main/java/com/cmoney/backend2/vtwebapi/service/api/createaccount/AccountType.kt
@@ -2,11 +2,15 @@ package com.cmoney.backend2.vtwebapi.service.api.createaccount
 
 /**
  * 創建帳戶類型
- *
- * @property STOCK 股票
- * @property OPTIONS 期權
  */
 enum class AccountType(val typeNum: Int) {
+    /**
+     * 股票
+     */
     STOCK(1),
+
+    /**
+     * 期權
+     */
     OPTIONS(2)
 }
--- backend2/src/main/java/com/cmoney/backend2/vtwebapi/service/api/getcardinstancesns/UsageType.kt
+++ backend2/src/main/java/com/cmoney/backend2/vtwebapi/service/api/getcardinstancesns/UsageType.kt
@@ -2,15 +2,25 @@ package com.cmoney.backend2.vtwebapi.service.api.getcardinstancesns
 
 /**
  * 使用道具類型
- *
- * @property UNUSED 未使用
- * @property IS_USING 使用中
- * @property USE_UP 使用完畢
- * @property REFUND 退款取消
  */
 enum class UsageType(val typeNum: Int) {
+    /**
+     * 未使用
+     */
     UNUSED(1),
+
+    /**
+     * 使用中
+     */
     IS_USING(2),
+
+    /**
+     * 使用完畢
+     */
     USE_UP(3),
+
+    /**
+     * 退款取消
+     */
     REFUND(5)
 }
