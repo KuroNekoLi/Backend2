c19c0f8a4410f70be69fdcdd5b06c9cf21d3b1f3
Willie_Chang <willie_chang@cmoney.com.tw>
2022-04-22 15:49:34 +0800

    feat: add AdditionalInformationRevisit YesterdayData api

--- backend2/src/main/java/com/cmoney/backend2/additioninformationrevisit/service/AdditionalInformationRevisitService.kt
+++ backend2/src/main/java/com/cmoney/backend2/additioninformationrevisit/service/AdditionalInformationRevisitService.kt
@@ -4,7 +4,11 @@ import com.cmoney.backend2.additioninformationrevisit.service.api.request.GetReq
 import com.cmoney.backend2.additioninformationrevisit.service.api.request.RequestIds
 import com.cmoney.backend2.base.model.calladapter.RecordApi
 import retrofit2.Response
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+import retrofit2.http.Url
 
 interface AdditionalInformationRevisitService {
 
@@ -52,5 +56,43 @@ interface AdditionalInformationRevisitService {
         @Url url: String,
         @Header("Authorization") authorization: String,
         @Body param: RequestIds
+    ): Response<List<List<String?>>>
+
+    @RecordApi
+    @POST
+    suspend fun getYesterdayAll(
+        @Url url: String,
+        @Header("Authorization") authorization: String,
+        @Query("columns") columns: String,
+        @Body param: GetRequestParam
+    ): Response<List<List<String?>>>
+
+    @RecordApi
+    @POST
+    suspend fun getYesterdayTarget(
+        @Url url: String,
+        @Header("Authorization") authorization: String,
+        @Query("keyNamePath") keyNamePath: String,
+        @Query("columns") columns: String,
+        @Body param: GetRequestParam
+    ): Response<List<List<String?>>>
+
+    @RecordApi
+    @POST
+    suspend fun getYesterdayMultiple(
+        @Url url: String,
+        @Header("Authorization") authorization: String,
+        @Query("columns") columns: String,
+        @Query("keyNamePath") keyNamePath: String,
+        @Body param: GetRequestParam
+    ): Response<List<List<String?>>>
+
+    @RecordApi
+    @POST
+    suspend fun getYesterdayOtherQuery(
+        @Url url: String,
+        @Header("Authorization") authorization: String,
+        @Query("columns") columns: String,
+        @Body param: GetRequestParam
     ): Response<List<List<String?>>>
 }
--- backend2/src/main/java/com/cmoney/backend2/additioninformationrevisit/service/ServicePath.kt
+++ backend2/src/main/java/com/cmoney/backend2/additioninformationrevisit/service/ServicePath.kt
@@ -8,6 +8,10 @@ package com.cmoney.backend2.additioninformationrevisit.service
  * @property multiple String [AdditionalInformationRevisitService.getMultiple]的service參數
  * @property otherQuery String [AdditionalInformationRevisitService.getOtherQuery]的service參數
  * @property signal String [AdditionalInformationRevisitService.getSignal]的service參數
+ * @property yesterdayAll String [AdditionalInformationRevisitService.getYesterdayAll]的service參數
+ * @property yesterdayTarget String [AdditionalInformationRevisitService.getYesterdayTarget]的service參數
+ * @property yesterdayMultiple String [AdditionalInformationRevisitService.getYesterdayMultiple]的service參數
+ * @property yesterdayOtherQuery String [AdditionalInformationRevisitService.getYesterdayOtherQuery]的service參數
  * @constructor
  */
 data class ServicePath(
@@ -15,14 +19,22 @@ data class ServicePath(
     val target: String,
     val multiple: String,
     val otherQuery: String,
-    val signal: String
+    val signal: String,
+    val yesterdayAll: String,
+    val yesterdayTarget: String,
+    val yesterdayMultiple: String,
+    val yesterdayOtherQuery: String
 ) {
     constructor(serviceName: String = PRODUCT_SERVICE_NAME) : this(
         all = serviceName,
         target = serviceName,
         multiple = serviceName,
         otherQuery = serviceName,
-        signal = serviceName
+        signal = serviceName,
+        yesterdayAll = serviceName,
+        yesterdayTarget = serviceName,
+        yesterdayMultiple = serviceName,
+        yesterdayOtherQuery = serviceName
     )
 
     companion object {
