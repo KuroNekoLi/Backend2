6235433b1ec1ce6099d9d1825bad8a315e5fc67c
Larry Hsiao <larry_hsiao@cmoney.com.tw>
2022-07-08 11:41:08 +0800

    chore: Update dto of board

--- backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/group/v2/BoardDTO.kt
+++ backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/group/v2/BoardDTO.kt
@@ -1,11 +1,13 @@
 package com.cmoney.backend2.forumocean.service.api.group.v2
 
 
-import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
+import androidx.annotation.Keep
 
 @Keep
 data class BoardDTO(
+    @SerializedName("boardType")
+    val boardType: String?,
     @SerializedName("canRead")
     val canRead: Boolean?,
     @SerializedName("id")
