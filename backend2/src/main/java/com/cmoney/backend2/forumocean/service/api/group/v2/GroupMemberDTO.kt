506a39a313d82faccb49d95993ba28c0a8e9dbf4
Larry Hsiao <larry_hsiao@cmoney.com.tw>
2022-07-15 11:26:06 +0800

    chore: Annotation for GroupMemberDTO

--- backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/group/v2/GroupMemberDTO.kt
+++ backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/group/v2/GroupMemberDTO.kt
@@ -1,8 +1,16 @@
 package com.cmoney.backend2.forumocean.service.api.group.v2
 
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
 data class GroupMemberDTO(
+    @SerializedName("image")
     val image: String,
+    @SerializedName("memberId")
     val memberId: Int,
+    @SerializedName("nickName")
     val nickName: String,
+    @SerializedName("roleIds")
     val roleIds: List<Int>
 )
