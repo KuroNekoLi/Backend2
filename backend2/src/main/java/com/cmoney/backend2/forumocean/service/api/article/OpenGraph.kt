2e89a877310f832d4b9e516c681fbd6b03eb043c
Larry Hsiao <larry_hsiao@cmoney.com.tw>
2022-08-19 14:45:38 +0800

    chore: Doc for OpenGraph.

--- backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/article/OpenGraph.kt
+++ backend2/src/main/java/com/cmoney/backend2/forumocean/service/api/article/OpenGraph.kt
@@ -2,6 +2,15 @@ package com.cmoney.backend2.forumocean.service.api.article
 
 import com.google.gson.annotations.SerializedName
 
+/**
+ * OpenGraph
+ *
+ * @property description OpenGraph 描述
+ * @property image 圖片url
+ * @property siteName 網站名稱
+ * @property title 標題
+ * @property url 轉導向url
+ */
 data class OpenGraph(
     @SerializedName("description")
     val description: String?,
