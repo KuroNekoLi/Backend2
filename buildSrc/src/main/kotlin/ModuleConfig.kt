e90e6e4cc3a496fab44ef5db5b998b49bee4427a
Asher <asher_wu@cmoney.com.tw>
2023-04-28 08:36:06 +0800

    chore: 修正版號為5.50.2

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,7 +1,7 @@
 object ModuleConfig {
-    const val PROJECT_VERSION = "5.50.2.999"
+    const val PROJECT_VERSION = "5.50.2"
     // Maven Upload Properties
-    const val RELEASE_URL = "http://192.168.99.70:8081/repository/maven-snapshots/"
+    const val RELEASE_URL = "http://192.168.99.70:8081/repository/maven-releases/"
     const val USER_NAME = "uploader"
     const val USER_PASSWORD = "g.3ruyj3"
     // Publish Config
