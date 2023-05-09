943fef4e59a4d7cc2c71deff108a39654d9df809
Willie_Chang <willie_chang@cmoney.com.tw>
2023-05-09 17:55:53 +0800

    feat: ClientConfigurationWeb加入SettingAdapter，ClientConfigurationService 方法改為使用 @Url 發送

--- backend2/src/main/java/com/cmoney/backend2/clientconfiguration/service/ClientConfigurationWebImpl.kt
+++ backend2/src/main/java/com/cmoney/backend2/clientconfiguration/service/ClientConfigurationWebImpl.kt
@@ -1,6 +1,7 @@
 package com.cmoney.backend2.clientconfiguration.service
 
 import com.cmoney.backend2.base.extension.checkResponseBody
+import com.cmoney.backend2.base.model.manager.GlobalBackend2Manager
 import com.cmoney.backend2.clientconfiguration.service.api.ClientConfigInfoParser
 import com.cmoney.backend2.clientconfiguration.service.api.ClientConfigType
 import com.cmoney.backend2.clientconfiguration.service.api.ConfigKey
@@ -10,21 +11,28 @@ import com.google.gson.Gson
 import kotlinx.coroutines.withContext
 
 class ClientConfigurationWebImpl(
+    override val manager: GlobalBackend2Manager,
     private val service: ClientConfigurationService,
     private val jsonParser: Gson,
     private val dispatcher: DispatcherProvider = DefaultDispatcherProvider
 ) : ClientConfigurationWeb {
 
-    override suspend fun getConfig(list: List<ConfigKey>): Result<List<ClientConfigType>> =
+    override suspend fun getConfig(
+        list: List<ConfigKey>,
+        domain: String,
+        url: String
+    ): Result<List<ClientConfigType>> =
         withContext(dispatcher.io()) {
             kotlin.runCatching {
                 val keyList = list.map { it.stringValue }
-                val response = service.getConfig(keyList)
+                val response = service.getConfig(
+                    url = url,
+                    keys = keyList
+                )
                 val responseBody = response.checkResponseBody(jsonParser)
                 responseBody.configs.mapNotNull {
                     ClientConfigInfoParser.getClientConfigType(it, jsonParser)
                 }
             }
         }
-
 }
