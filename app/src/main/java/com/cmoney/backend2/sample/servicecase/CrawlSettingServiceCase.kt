c5ce79d92a7e8a1b81392b1566d9b5e3acf4b908
fu_chen <fu_chen@cmoney.com.tw>
2023-05-10 14:40:32 +0800

    feat: CrawlSettingWeb加入SettingAdapter(Breaking)

--- app/src/main/java/com/cmoney/backend2/sample/servicecase/CrawlSettingServiceCase.kt
+++ app/src/main/java/com/cmoney/backend2/sample/servicecase/CrawlSettingServiceCase.kt
@@ -1,27 +1,21 @@
 package com.cmoney.backend2.sample.servicecase
 
-import com.cmoney.backend2.crawlsetting.di.BACKEND2_CRAWL_SETTING_DEBUG
 import com.cmoney.backend2.crawlsetting.service.CrawlSettingWeb
 import com.cmoney.backend2.sample.extension.logResponse
 import org.koin.core.component.inject
-import org.koin.core.qualifier.StringQualifier
 
-class CrawlSettingServiceCase(
-    qualifier: StringQualifier = BACKEND2_CRAWL_SETTING_DEBUG
-) : ServiceCase {
+class CrawlSettingServiceCase() : ServiceCase {
 
-    private val web by inject<CrawlSettingWeb>(qualifier)
+    private val web by inject<CrawlSettingWeb>()
 
     override suspend fun testAll() {
         web.getCathayCaStatus(
             userInfoKey = "B7828223EEAC33D284AF092851564BB61D598352E54E666DC1B5F6100BFD0438"
-        )
-            .logResponse(TAG)
+        ).logResponse(TAG)
 
         web.getTaishinCaStatus(
             userInfoKey = "27214C83DF796BD61C18409D9C91094D9D2E4125B1DE2C2B0D217409602F5EF8"
-        )
-            .logResponse(TAG)
+        ).logResponse(TAG)
     }
 
     companion object {
