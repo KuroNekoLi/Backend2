96e967a0a0d3314315bfbf7259cc717257dbe335
Willie_Chang <willie_chang@cmoney.com.tw>
2023-05-10 15:45:21 +0800

    doc: 補 ChipKWeb 方法參數註解

--- backend2/src/main/java/com/cmoney/backend2/chipk/service/ChipKWeb.kt
+++ backend2/src/main/java/com/cmoney/backend2/chipk/service/ChipKWeb.kt
@@ -15,6 +15,8 @@ interface ChipKWeb {
      * 服務6-2. 籌碼K統一要求Dtno的方法
      * @param commKey 股票代號
      * @param type 1-KD, 2-MACD, 3-外資成本線, 4-持股比率, 5-月營收, 6-獲利
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getData(
@@ -27,6 +29,8 @@ interface ChipKWeb {
     /**
      * 服務6-6. 要求大盤外資的資料(TWA00)
      * @param tickCount 根數
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getIndexForeignInvestment(
@@ -38,6 +42,8 @@ interface ChipKWeb {
     /**
      * 服務6-7. 要求大盤主力的資料(TWA00)
      * @param tickCount 根數
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getIndexMain(
@@ -49,6 +55,8 @@ interface ChipKWeb {
     /**
      * 服務6-8. 要求大盤資券資料(TWA00)
      * @param tickCount 根數
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getIndexFunded(
@@ -62,6 +70,8 @@ interface ChipKWeb {
      *
      * @param id 商品代號
      * @param productType 商品類型
+     * @param domain 網域名稱
+     * @param url 完整的Url
      */
     suspend fun getInternationalKChart(
         id: String,
@@ -72,6 +82,9 @@ interface ChipKWeb {
 
     /**
      * 服務6-10. 取得加權指數融資維持率
+     *
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getCreditRate(
@@ -81,8 +94,11 @@ interface ChipKWeb {
 
     /**
      * 服務6-11. 取得指數技術圖
+     *
      * @param commKey 股票代號
      * @param timeRange 1-日, 2-週, 3-月
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getIndexCalculateRate(
@@ -94,8 +110,11 @@ interface ChipKWeb {
 
     /**
      * 服務6-12. 取得K圖資料
+     *
      * @param commKey 股票代號
      * @param timeRange 1-日, 2-週, 3-月
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 請使用DtnoData.toListOfSomething 轉換成自定義的物件
      */
     suspend fun getIndexKData(
@@ -107,8 +126,11 @@ interface ChipKWeb {
 
     /**
      * 服務4-1. 向國鳴用封包要服務(已無使用)
+     *
      * @param fundId 服務編號 (參見 資料輸出入SPEC_20XXXXXX.doc)
      * @param params 服務的參數 (用底線來分開)
+     * @param domain 網域名稱
+     * @param url 完整的Url
      */
     suspend fun getChipKData(
         fundId: Int,
@@ -119,6 +141,11 @@ interface ChipKWeb {
 
     /**
      * 取得官方資料
+     *
+     * @param index 索引位置
+     * @param pageType ???
+     * @param domain 網域名稱
+     * @param url 完整的Url
      */
     suspend fun getOfficialStockPickData(
         index: Int,
@@ -129,6 +156,10 @@ interface ChipKWeb {
 
     /**
      * 取得官方標題
+     *
+     * @param type ???
+     * @param domain 網域名稱
+     * @param url 完整的Url
      */
     suspend fun getOfficialStockPickTitle(
         type: Int,
@@ -140,6 +171,9 @@ interface ChipKWeb {
      * 期貨盤後資訊
      * 服務 - 官股、融資
      * 取得盤後官股、融資變動以及三大法人買賣超
+     *
+     * @param domain 網域名稱
+     * @param url 完整的Url
      */
     suspend fun getFutureDayTradeIndexAnalysis(
         domain: String = manager.getChipKSettingAdapter().getDomain(),
