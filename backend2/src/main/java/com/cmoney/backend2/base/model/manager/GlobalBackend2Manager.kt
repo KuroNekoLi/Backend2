87a8741b92fcd7ded2845d238a81156faa4b0087
Willie_Chang <willie_chang@cmoney.com.tw>
2023-05-11 17:11:03 +0800

    feat: MediaWeb加入SettingAdapter, MediaService改用@Url發送
    BREAKING-CHANGE: MediaWeb 移除有 ApiParam 參數之方法，MediaService 參數新增 @Url

--- backend2/src/main/java/com/cmoney/backend2/base/model/manager/GlobalBackend2Manager.kt
+++ backend2/src/main/java/com/cmoney/backend2/base/model/manager/GlobalBackend2Manager.kt
@@ -43,6 +43,8 @@ import com.cmoney.backend2.emilystock.model.EmilyStockSettingAdapter
 import com.cmoney.backend2.emilystock.model.EmilyStockSettingAdapterImpl
 import com.cmoney.backend2.frontendlogger.model.FrontEndLoggerSettingAdapter
 import com.cmoney.backend2.frontendlogger.model.FrontEndLoggerSettingAdapterImpl
+import com.cmoney.backend2.media.model.MediaSettingAdapter
+import com.cmoney.backend2.media.model.MediaSettingAdapterImpl
 import com.cmoney.backend2.virtualtrading2.model.settingadapter.VirtualTrading2SettingAdapter
 import com.cmoney.backend2.virtualtrading2.model.settingadapter.VirtualTrading2SettingAdapterImpl
 import com.cmoney.backend2.vtwebapi.model.settingadapter.VirtualTradeSettingAdapter
@@ -72,6 +74,7 @@ import com.cmoney.backend2.vtwebapi.model.settingadapter.VirtualTradeSettingAdap
  * @property dataSettingAdapter Data服務設定轉接器
  * @property emilyStockSettingAdapter EmilyStock服務設定轉接器
  * @property frontEndLoggerSettingAdapter FrontEndLogger服務設定轉接器
+ * @property mediaSettingAdapter MobileService-Media服務設定轉接器
  * @property virtualTradeSettingAdapter 虛擬下單V1轉接器
  * @property virtualTrading2SettingAdapter 虛擬下單V2轉接器
  *
@@ -98,6 +101,7 @@ class GlobalBackend2Manager(
     private val dataSettingAdapter: DataSettingAdapter,
     private val emilyStockSettingAdapter: EmilyStockSettingAdapter,
     private val frontEndLoggerSettingAdapter: FrontEndLoggerSettingAdapter,
+    private val mediaSettingAdapter: MediaSettingAdapter,
     private val virtualTradeSettingAdapter: VirtualTradeSettingAdapter,
     private val virtualTrading2SettingAdapter: VirtualTrading2SettingAdapter,
 ) {
@@ -123,6 +127,7 @@ class GlobalBackend2Manager(
         dataSettingAdapter = builder.dataSettingAdapter,
         emilyStockSettingAdapter = builder.emilyStockSettingAdapter,
         frontEndLoggerSettingAdapter = builder.frontEndLoggerSettingAdapter,
+        mediaSettingAdapter = builder.mediaSettingAdapter,
         virtualTradeSettingAdapter = builder.virtualTradeSettingAdapter,
         virtualTrading2SettingAdapter = builder.virtualTrading2SettingAdapter
     )
@@ -410,6 +415,15 @@ class GlobalBackend2Manager(
     }
 
     /**
+     * 取得MobileService-Media服務設定轉接器
+     *
+     * @return MobileService-Media服務設定轉接器
+     */
+    fun getMediaSettingAdapter(): MediaSettingAdapter {
+        return mediaSettingAdapter
+    }
+
+    /**
      * 取得虛擬下單V1設定轉接器
      */
     fun getVirtualTradeSettingAdapter(): VirtualTradeSettingAdapter {
@@ -498,6 +512,8 @@ class GlobalBackend2Manager(
             EmilyStockSettingAdapterImpl(backendSetting)
         var frontEndLoggerSettingAdapter: FrontEndLoggerSettingAdapter =
             FrontEndLoggerSettingAdapterImpl(backendSetting)
+        var mediaSettingAdapter: MediaSettingAdapter =
+            MediaSettingAdapterImpl(backendSetting)
         var virtualTradeSettingAdapter: VirtualTradeSettingAdapter =
             VirtualTradeSettingAdapterImpl(backendSetting)
         var virtualTrading2SettingAdapter: VirtualTrading2SettingAdapter =
--- backend2/src/main/java/com/cmoney/backend2/media/service/MediaWeb.kt
+++ backend2/src/main/java/com/cmoney/backend2/media/service/MediaWeb.kt
@@ -1,6 +1,6 @@
 package com.cmoney.backend2.media.service
 
-import com.cmoney.backend2.base.model.request.MemberApiParam
+import com.cmoney.backend2.base.model.manager.GlobalBackend2Manager
 import com.cmoney.backend2.media.service.api.getlivevideolistbyappid.LiveStreamInfo
 import com.cmoney.backend2.media.service.api.getmediadetail.GetMediaDetailResponse
 import com.cmoney.backend2.media.service.api.getmediainfo.MediaInfo
@@ -11,139 +11,165 @@ import com.cmoney.backend2.media.service.api.getpaidmedialistofmember.BoughtMedi
 import com.cmoney.backend2.media.service.api.getpaidmedialistofmemberbyappid.Media
 
 interface MediaWeb {
-    @Deprecated("ApiParam no longer required")
+
+    val manager: GlobalBackend2Manager
+
+    /**
+     * 取得手機影音清單
+     *
+     * @param skipCount 略過數量(曾經要求過的數量)
+     * @param fetchCount 要求數量
+     * @param chargeType 0: All, 1: Paid, 2: Free
+     * @param tagIdList 標籤集合
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 手機影音清單
+     */
     suspend fun getMediaList(
-        apiParam: MemberApiParam,
         skipCount: Int,
         fetchCount: Int,
         chargeType: Int,
-        tagIdList : List<Int> = emptyList()
+        tagIdList : List<Int> = emptyList(),
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<List<VideoInfo>>
 
-    suspend fun getMediaList(
+    /**
+     * 取得影音購買網址
+     *
+     * @param mediaId 影音編號
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 影音購買網址
+     */
+    suspend fun getMediaPurchaseUrl(
+        mediaId: Long,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
+    ): Result<String>
+
+    /**
+     * 取得影音播放網址
+     *
+     * @param mediaId 影音編號
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 影音播放網址
+     */
+    suspend fun getMediaUrl(
+        mediaId: Long,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
+    ): Result<String>
+
+    /**
+     * 取得手機直播清單
+     *
+     * @param skipCount 略過數量(曾經要求過的數量)
+     * @param fetchCount 要求數量
+     * @param chargeType 0: All, 1: Paid, 2: Free
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 手機直播清單
+     */
+    suspend fun getLiveStreamList(
         skipCount: Int,
         fetchCount: Int,
         chargeType: Int,
-        tagIdList : List<Int> = emptyList()
-    ): Result<List<VideoInfo>>
-
-    @Deprecated("ApiParam no longer required")
-    suspend fun getMediaPurchaseUrl(apiParam: MemberApiParam, mediaId: Long): Result<String>
-
-    suspend fun getMediaPurchaseUrl(mediaId: Long): Result<String>
-
-    @Deprecated("ApiParam no longer required")
-    suspend fun getMediaUrl(apiParam: MemberApiParam, mediaId: Long): Result<String>
-    suspend fun getMediaUrl(mediaId: Long): Result<String>
-
-    @Deprecated("ApiParam no longer required")
-    suspend fun getLiveStreamList(
-        apiParam: MemberApiParam,
-        skipCount: Int,
-        fetchCount: Int,
-        chargeType: Int
-    ): Result<List<LiveStreamInfo>>
-
-    suspend fun getLiveStreamList(
-        skipCount: Int,
-        fetchCount: Int,
-        chargeType: Int
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<List<LiveStreamInfo>>
 
     /**
      * 取得會員已購買的影音清單
-     * @param apiParam guid,authToken,appId
+     *
      * @param skipCount 略過筆數
      * @param fetchCount 欲取得的資料筆數
-     * @return 購買的影音清單的result
-     */
-    @Deprecated("ApiParam no longer required")
-    suspend fun getPaidMediaListOfMember(
-        apiParam: MemberApiParam,
-        skipCount: Int,
-        fetchCount: Int
-    ): Result<List<BoughtMediaListInfo>>
-
-    /**
-     * 取得會員已購買的影音清單
-     * @param skipCount 略過筆數
-     * @param fetchCount 欲取得的資料筆數
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 購買的影音清單的result
      */
     suspend fun getPaidMediaListOfMember(
         skipCount: Int,
-        fetchCount: Int
+        fetchCount: Int,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<List<BoughtMediaListInfo>>
 
     /**
      * 取得付費影音清單
-     * @param apiParam guid,authToken,appId
+     *
      * @param skipCount 略過筆數
      * @param fetchCount 欲取得的資料筆數
-     * @return 付費影音清單的result
-     */
-    @Deprecated("ApiParam no longer required")
-    suspend fun getPaidMediaList(
-        apiParam: MemberApiParam,
-        skipCount: Int,
-        fetchCount: Int
-    ): Result<List<PaidMediaListInfo>>
-
-    /**
-     * 取得付費影音清單
-     * @param skipCount 略過筆數
-     * @param fetchCount 欲取得的資料筆數
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 付費影音清單的result
      */
     suspend fun getPaidMediaList(
         skipCount: Int,
-        fetchCount: Int
+        fetchCount: Int,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<List<PaidMediaListInfo>>
 
     /**
      * 取得付費直播清單
-     * @param apiParam guid,authToken,appId
+     *
      * @param skipCount 略過筆數
      * @param fetchCount 欲取得的資料筆數
-     * @return 付費直播清單的result
-     */
-    @Deprecated("ApiParam no longer required")
-    suspend fun getPaidLiveList(
-        apiParam: MemberApiParam,
-        skipCount: Int,
-        fetchCount: Int
-    ): Result<List<PaidLiveListInfo>>
-
-
-    /**
-     * 取得付費直播清單
-     * @param apiParam guid,authToken,appId
-     * @param skipCount 略過筆數
-     * @param fetchCount 欲取得的資料筆數
+     * @param domain 網域名稱
+     * @param url 完整的Url
      * @return 付費直播清單的result
      */
     suspend fun getPaidLiveList(
         skipCount: Int,
-        fetchCount: Int
+        fetchCount: Int,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<List<PaidLiveListInfo>>
-
-    @Deprecated("ApiParam no longer required.")
-    suspend fun getMediaInfo(
-        apiParam: MemberApiParam,
-        mediaId: Long
-    ): Result<MediaInfo>
 
     /**
      * 取得影音詳情內容
+     *
+     * @param mediaId 影音編號
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 影音詳情內容
      */
     suspend fun getMediaInfo(
-        mediaId: Long
+        mediaId: Long,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<MediaInfo>
 
-    suspend fun getMediaDetail(mediaId: Long): Result<GetMediaDetailResponse>
+    /**
+     * 取得影音詳細資訊(有單元課程的影音才會用)-已廢除
+     *
+     * @param mediaId 影音編號
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 影音詳細資訊
+     */
+    @Deprecated(message = "改為使用 getMediaInfo()")
+    suspend fun getMediaDetail(
+        mediaId: Long,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
+    ): Result<GetMediaDetailResponse>
 
+    /**
+     * 取得指定app的會員已購買的影音清單
+     *
+     * @param skipCount 略過數量(曾經要求過的數量)
+     * @param fetchCount 要求數量
+     * @param domain 網域名稱
+     * @param url 完整的Url
+     * @return 影音清單
+     */
     suspend fun getPaidMediaListOfMemberByAppId(
         skipCount: Int,
-        fetchCount: Int
+        fetchCount: Int,
+        domain: String = manager.getMediaSettingAdapter().getDomain(),
+        url: String = "${domain}MobileService/ashx/Media/Media.ashx"
     ): Result<List<Media>>
 }
