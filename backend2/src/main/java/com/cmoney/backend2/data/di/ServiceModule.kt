2cecf31d047380b7d234ecbfa0556e10ab8bc439
Willie_Chang <willie_chang@cmoney.com.tw>
2023-05-11 10:52:58 +0800

    feat: DataWeb加入SettingAdapter(Breaking), DataService使用@Query帶入參數

--- backend2/src/main/java/com/cmoney/backend2/data/di/ServiceModule.kt
+++ backend2/src/main/java/com/cmoney/backend2/data/di/ServiceModule.kt
@@ -1,65 +1,20 @@
 package com.cmoney.backend2.data.di
 
-import com.cmoney.backend2.base.di.BACKEND2_RETROFIT
-import com.cmoney.backend2.base.di.BACKEND2_SETTING
-import com.cmoney.backend2.base.di.addLogInterceptor
+import com.cmoney.backend2.base.di.BACKEND2_RETROFIT_V2
 import com.cmoney.backend2.data.service.DataService
 import com.cmoney.backend2.data.service.DataWeb
 import com.cmoney.backend2.data.service.DataWebImpl
-import okhttp3.Call
-import okhttp3.ConnectionSpec
-import okhttp3.OkHttpClient
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import java.util.concurrent.TimeUnit
 
-typealias RetrofitProvider = () -> Retrofit
-
-fun dataServiceModule(retrofitProvider: RetrofitProvider) = module {
-    single<DataService> {
-        retrofitProvider.invoke().create(DataService::class.java)
+val dataServiceModule = module {
+    single {
+        get<Retrofit>(BACKEND2_RETROFIT_V2).create(DataService::class.java)
     }
     single<DataWeb> {
         DataWebImpl(
-            service = get(),
-            setting = get(BACKEND2_SETTING)
+            manager = get(),
+            service = get()
         )
     }
-}
-
-private val DATA_RETROFIT = named("cmoney_backend_data_retrofit")
-
-val dataServiceModule = module {
-    single(DATA_RETROFIT) {
-        val backend2Retrofit = get<Retrofit>(BACKEND2_RETROFIT)
-        val retrofitBuilder = backend2Retrofit.newBuilder()
-        // 移除動態切換Host Interceptor
-        retrofitBuilder
-            .callFactory(createChatOkHttpClient(backend2Retrofit.callFactory()))
-            .build()
-    }
-    single {
-        get<Retrofit>(DATA_RETROFIT).create(DataService::class.java)
-    }
-    single<DataWeb> {
-        DataWebImpl(service = get(), setting = get(BACKEND2_SETTING))
-    }
-}
-
-private fun createChatOkHttpClient(baseCallFactory: Call.Factory): OkHttpClient {
-    val okHttpClientBuilder = if (baseCallFactory is OkHttpClient) {
-        with(baseCallFactory.newBuilder()) {
-            interceptors().clear()
-            addLogInterceptor()
-        }
-    } else {
-        OkHttpClient.Builder()
-            .connectionSpecs(listOf(ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.CLEARTEXT))
-            .connectTimeout(30L, TimeUnit.SECONDS)
-            .callTimeout(30L, TimeUnit.SECONDS)
-            .readTimeout(30L, TimeUnit.SECONDS)
-            .writeTimeout(30L, TimeUnit.SECONDS)
-    }
-    return okHttpClientBuilder.build()
 }
