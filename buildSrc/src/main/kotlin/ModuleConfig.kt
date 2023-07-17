8e16674cb717f63ce7c22018a5e7d45654408fff
Willie_Chang <willie_chang@cmoney.com.tw>
2023-07-17 09:09:52 +0800

    build: 5.53.3 > 5.53.4

--- buildSrc/src/main/kotlin/ModuleConfig.kt
+++ buildSrc/src/main/kotlin/ModuleConfig.kt
@@ -1,5 +1,5 @@
 object ModuleConfig {
-    const val PROJECT_VERSION = "5.53.3"
+    const val PROJECT_VERSION = "5.53.4"
     // Maven Upload Properties
     const val RELEASE_URL = "http://192.168.99.70:8081/repository/maven-releases/"
     const val USER_NAME = "uploader"
